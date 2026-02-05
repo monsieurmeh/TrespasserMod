2a4c8e1f5c6e40e5a01c8c3c2eb3ebc9db1050ea
MonsieurMeh <mensoirmeh@gmail.com>
2026-02-04 23:03:05 -0800

    patch preventing game from overwriting my neatly arranged gamemode with freaking NULL. thanks game! Also some cleanup and basic org on sanbox config management

--- Patches.cs
+++ Patches.cs
@@ -1,5 +1,6 @@
 ﻿using HarmonyLib;
 using Il2Cpp;
+using Il2CppTLD.AI;
 using Il2CppTLD.Gameplay;
 using MehToolBox;
 using MelonLoader;
@@ -9,15 +10,12 @@ namespace Trespasser
 {
     internal class Patches
     {
-        static SandboxConfig TrespasserConfig;
-
         [HarmonyPatch(typeof(Panel_SelectExperience), nameof(Panel_SelectExperience.Initialize))]
         internal static class PanelSelectExperience_AddTrespasserMode
         {
             public static void Prefix(ref Panel_SelectExperience __instance)
             {
-                SandboxConfig trespasserConfig = new SandboxConfig();
-                ConfigureSandbox(trespasserConfig, __instance.m_MenuItems[2].m_SandboxConfig, __instance.m_MenuItems[3].m_SandboxConfig);
+                SandboxConfig trespasserConfig = SandboxConfigManager.TrespasserConfig;
 
                 GameObject trespasserDisplay = UnityEngine.Object.Instantiate(__instance.m_MenuItems[2].m_Display);
                 ConfigureDisplay(trespasserDisplay, __instance.m_MenuItems[2].m_Display, __instance.m_MenuItems[3].m_Display);
@@ -29,87 +27,17 @@ namespace Trespasser
                     m_Display = trespasserDisplay,
                     m_SandboxConfig = trespasserConfig
                 });
-
-                TrespasserConfig = trespasserConfig;
-            }
-
-            #region sandbox
-
-            private static void ConfigureSandbox(SandboxConfig trespasser, SandboxConfig stalker, SandboxConfig interloper)
-            {
-                ConfigureSandboxGeneral(trespasser, stalker, interloper);
-                ConfigureSandboxBunkerSetup(trespasser, stalker, interloper);
-                ConfigureSandboxXPMode(trespasser, stalker, interloper);
-                ConfigureSandboxStartGear(trespasser, stalker, interloper);
-                ConfigureSandboxAvailableRegions(trespasser, stalker, interloper);
-                ConfigureSandboxSceneLoadConditions(trespasser, stalker, interloper);
-
-                MehToolBox.ScriptExaminer.Compare(trespasser, stalker);
-            }
-
-            private static void ConfigureSandboxGeneral(SandboxConfig trespasser, SandboxConfig stalker, SandboxConfig interloper)
-            {
-                trespasser.name = "Trespasser";
-                trespasser.m_NumFeats = 2;
-                trespasser.m_ForceSpawnPoint = string.Empty;
-                trespasser.m_MissionServicesPrefab = stalker.m_MissionServicesPrefab;
-                trespasser.m_ModeName = new LocalizedString() { m_LocalizationID = "Trespasser" }; //should be GAMEPLAY_Trespasser
-                trespasser.m_Description = new LocalizedString() { m_LocalizationID = "Trespasser Description" };
-                trespasser.m_LoadingText = new LocalizedString() { m_LocalizationID = "Trespasser Loading Text" };
-                trespasser.m_SpriteName = stalker.m_SpriteName;
-                trespasser.m_ActiveTags = stalker.m_ActiveTags;
-                trespasser.m_SaveSlotType = SaveSlotType.SANDBOX;
-            }
-
-            private static void ConfigureSandboxBunkerSetup(SandboxConfig trespasser, SandboxConfig stalker, SandboxConfig interloper)
-            {
-                trespasser.m_BunkerSetup = UnityEngine.Object.Instantiate(stalker.m_BunkerSetup);
             }
 
 
-            private static void ConfigureSandboxXPMode(SandboxConfig trespasser, SandboxConfig stalker, SandboxConfig interloper)
-            {
-                trespasser.m_XPMode = UnityEngine.Object.Instantiate(stalker.m_XPMode);
-            }
-
-
-            private static void ConfigureSandboxStartGear(SandboxConfig trespasser, SandboxConfig stalker, SandboxConfig interloper)
-            {
-                trespasser.m_XPMode.m_StartGear = UnityEngine.Object.Instantiate(stalker.m_XPMode.m_StartGear);
-            }
-
-
-            private static void ConfigureSandboxAvailableRegions(SandboxConfig trespasser, SandboxConfig stalker, SandboxConfig interloper)
-            {
-                trespasser.m_AvailableStartRegions = new Il2CppInterop.Runtime.InteropTypes.Arrays.Il2CppReferenceArray<Il2CppTLD.Scenes.RegionSpecification>(interloper.m_AvailableStartRegions.Length);
-                for (int i = 0, iMax = interloper.m_AvailableStartRegions.Length; i < iMax; i++)
-                {
-                    trespasser.m_AvailableStartRegions[i] = UnityEngine.Object.Instantiate(interloper.m_AvailableStartRegions[i]);
-                }
-            }
-
-            private static void ConfigureSandboxSceneLoadConditions(SandboxConfig trespasser, SandboxConfig stalker, SandboxConfig interloper)
-            {
-                trespasser.m_LoadConditions = new Il2CppInterop.Runtime.InteropTypes.Arrays.Il2CppReferenceArray<Il2CppTLD.Scenes.SceneLoadCondition>(interloper.m_LoadConditions.Length);
-                for (int i = 0, iMax = interloper.m_LoadConditions.Length; i < iMax; i++)
-                {
-                    trespasser.m_LoadConditions[i] = UnityEngine.Object.Instantiate(interloper.m_LoadConditions[i]);
-                }
-            }
-
-            #endregion
-
-
-            #region display
-
-            private static void ConfigureDisplay(GameObject trespasser, GameObject stalker, GameObject interloper)
+            internal static void ConfigureDisplay(GameObject trespasser, GameObject stalker, GameObject interloper)
             {
                 ConfigureDisplayGeneral(trespasser,stalker,interloper);
                 ConfigureDisplayText(trespasser, stalker, interloper);
             }
 
 
-            private static void ConfigureDisplayGeneral(GameObject trespasser, GameObject stalker, GameObject interloper)
+            internal static void ConfigureDisplayGeneral(GameObject trespasser, GameObject stalker, GameObject interloper)
             {
                 trespasser.name = "TrespasserDifficultyDisplay";
                 trespasser.hideFlags = HideFlags.HideAndDontSave;
@@ -120,9 +48,6 @@ namespace Trespasser
             {
 
             }
- 
-
-            #endregion
         }
 
         // needed to provide runtime gamemodeconfig for savegames when trying to display for loading from main menu
@@ -134,8 +59,21 @@ namespace Trespasser
                 MelonLogger.Msg($"GameModeName: {gameModeName}");
                 if (gameModeName.Contains("Trespasser"))
                 {
-                    __result = TrespasserConfig;
+                    __result = SandboxConfigManager.TrespasserConfig;
                 }
+            }
+        }
+
+
+
+        [HarmonyPatch(typeof(ExperienceModeManager), nameof(ExperienceModeManager.SetGameModeConfig))]
+        internal static class ExperienceModeManager_SetGameModeConfigDebug
+        {
+            internal static bool Prefix(GameModeConfig gameModeConfig, ExperienceModeManager __instance)
+            {
+                return gameModeConfig != null
+                    || ExperienceModeManager.s_CurrentGameMode == null
+                    || !ExperienceModeManager.s_CurrentGameMode.m_ModeName.m_LocalizationID.Contains("Trespasser");
             }
         }
     }
---
+++ SandboxConfigManager.cs
@@ -0,0 +1,109 @@
+﻿using Il2Cpp;
+using Il2CppTLD.Gameplay;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trespasser
+{
+    internal class SandboxConfigManager
+    {
+        private static SandboxConfig _TrespasserConfig;
+
+        public static SandboxConfig TrespasserConfig
+        {
+            get
+            {
+                if (_TrespasserConfig == null)
+                {
+                    _TrespasserConfig = InstantiateSandbox();
+                }
+                return _TrespasserConfig;
+            }
+            set
+            {
+                _TrespasserConfig = value;
+            }
+        }
+
+        private static SandboxConfig InstantiateSandbox()
+        {
+            SandboxConfig trespasserConfig = new SandboxConfig();
+            SandboxConfig stalkerConfig = ExperienceModeManager.GetGameModeFromName("Stalker").Cast<SandboxConfig>();
+            SandboxConfig interloperConfig = ExperienceModeManager.GetGameModeFromName("Interloper").Cast<SandboxConfig>();
+            ConfigureSandbox(trespasserConfig, stalkerConfig, interloperConfig);
+            return trespasserConfig;
+        }
+
+
+        private static void ConfigureSandbox(SandboxConfig trespasser, SandboxConfig stalker, SandboxConfig interloper)
+        {
+            ConfigureSandboxGeneral(trespasser, stalker, interloper);
+            ConfigureSandboxBunkerSetup(trespasser, stalker, interloper);
+            ConfigureSandboxXPMode(trespasser, stalker, interloper);
+            ConfigureSandboxAvailableRegions(trespasser, stalker, interloper);
+            ConfigureSandboxSceneLoadConditions(trespasser, stalker, interloper);
+
+            MehToolBox.ScriptExaminer.Compare(trespasser, stalker);
+        }
+
+        private static void ConfigureSandboxGeneral(SandboxConfig trespasser, SandboxConfig stalker, SandboxConfig interloper)
+        {
+            trespasser.name = "Trespasser";
+            trespasser.m_NumFeats = 2;
+            trespasser.m_ForceSpawnPoint = string.Empty;
+            trespasser.m_MissionServicesPrefab = stalker.m_MissionServicesPrefab;
+            trespasser.m_ModeName = new LocalizedString() { m_LocalizationID = "Trespasser" }; //should be GAMEPLAY_Trespasser
+            trespasser.m_Description = new LocalizedString() { m_LocalizationID = "Trespasser Description" };
+            trespasser.m_LoadingText = new LocalizedString() { m_LocalizationID = "Trespasser Loading Text" };
+            trespasser.m_SpriteName = stalker.m_SpriteName;
+            trespasser.m_ActiveTags = stalker.m_ActiveTags;
+            trespasser.m_SaveSlotType = SaveSlotType.SANDBOX;
+        }
+
+        private static void ConfigureSandboxBunkerSetup(SandboxConfig trespasser, SandboxConfig stalker, SandboxConfig interloper)
+        {
+            trespasser.m_BunkerSetup = UnityEngine.Object.Instantiate(stalker.m_BunkerSetup);
+        }
+
+
+        private static void ConfigureSandboxXPMode(SandboxConfig trespasser, SandboxConfig stalker, SandboxConfig interloper)
+        {
+            trespasser.m_XPMode = UnityEngine.Object.Instantiate(stalker.m_XPMode);
+            ConfigureSandboxStartGear(trespasser, stalker, interloper);
+            ConfigureSandboxCougarSettings(trespasser, stalker, interloper);
+        }
+
+        private static void ConfigureSandboxStartGear(SandboxConfig trespasser, SandboxConfig stalker, SandboxConfig interloper)
+        {
+            trespasser.m_XPMode.m_StartGear = UnityEngine.Object.Instantiate(stalker.m_XPMode.m_StartGear);
+        }
+
+
+        private static void ConfigureSandboxCougarSettings(SandboxConfig trespasser, SandboxConfig stalker, SandboxConfig interloper)
+        {
+            trespasser.m_XPMode.m_CougarSettings = UnityEngine.Object.Instantiate(interloper.m_XPMode.m_CougarSettings);
+        }
+
+
+        private static void ConfigureSandboxAvailableRegions(SandboxConfig trespasser, SandboxConfig stalker, SandboxConfig interloper)
+        {
+            trespasser.m_AvailableStartRegions = new Il2CppInterop.Runtime.InteropTypes.Arrays.Il2CppReferenceArray<Il2CppTLD.Scenes.RegionSpecification>(interloper.m_AvailableStartRegions.Length);
+            for (int i = 0, iMax = interloper.m_AvailableStartRegions.Length; i < iMax; i++)
+            {
+                trespasser.m_AvailableStartRegions[i] = UnityEngine.Object.Instantiate(interloper.m_AvailableStartRegions[i]);
+            }
+        }
+
+        private static void ConfigureSandboxSceneLoadConditions(SandboxConfig trespasser, SandboxConfig stalker, SandboxConfig interloper)
+        {
+            trespasser.m_LoadConditions = new Il2CppInterop.Runtime.InteropTypes.Arrays.Il2CppReferenceArray<Il2CppTLD.Scenes.SceneLoadCondition>(interloper.m_LoadConditions.Length);
+            for (int i = 0, iMax = interloper.m_LoadConditions.Length; i < iMax; i++)
+            {
+                trespasser.m_LoadConditions[i] = UnityEngine.Object.Instantiate(interloper.m_LoadConditions[i]);
+            }
+        }
+    }
+}
