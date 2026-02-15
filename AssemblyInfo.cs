a90d9213c9f9a86f13c437c3ee95ce03d9ffd42e
MonsieurMeh <mensoirmeh@gmail.com>
2026-02-14 19:30:49 -0800

    1.0.0 release

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Reflection;
 
 [assembly: AssemblyTitle("Trespasser")]
 [assembly: AssemblyCopyright("MonsiuerMeh")]
-[assembly: AssemblyVersion("0.3.0")]
-[assembly: AssemblyFileVersion("0.3.0")]
-[assembly: MelonInfo(typeof(Trespasser.Main), "Trespasser", "0.3.0", "MonsieurMeh, Atmudia, Flower Field", null)]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: MelonInfo(typeof(Trespasser.Main), "Trespasser", "1.0.0", "MonsieurMeh, Atmudia, Flower Field", null)]
 [assembly: MelonGame("Hinterland", "TheLongDark")]
